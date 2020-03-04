3dc1b31412701280656201b7e1c5efa7c864e31d
Keturah Howard <keturah.dev@gmail.com>
2020-03-04 10:02:25 -0800

    create/fail test for sphere radius check

--- Shapes.Tests/ModelTests/SphereTests.cs
+++ Shapes.Tests/ModelTests/SphereTests.cs
@@ -16,5 +16,16 @@ namespace Shapes.Tests
       // assert
       Assert.AreEqual(typeof(Sphere), result.GetType());
     }
+    [TestMethod]
+    public void SphereConstructor_SphereHasRadiusFromCircle_SphereRadiusInt()
+    {
+      // arrange
+      Circle newCircle = new Circle(5);
+      Sphere newSphere = new Sphere(newCircle);
+      // act
+      int result = Sphere.Radius;
+      // assert
+      Assert.AreEqual(5, result);
+    }
   }
 }
